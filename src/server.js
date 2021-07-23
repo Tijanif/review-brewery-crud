5d469bf847e7e4780c912cf55212a25fe25ff375
Tijani Fulani <Fulani07@gmail.com>
2021-07-23 10:04:54 +0100

    breweries up an running

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const breweriesRouter = require('./resources/breweries/routes');
 
 const app = express();
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(morgan('dev'));
 
 // Routes
+app.use('/breweries', breweriesRouter);
 
 // running server
 app.listen(4000, () => {
